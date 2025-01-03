b9795fee0329323d383fc345132850522a1f8c1b
vvsupriyaalekhya <127216907+vvsupriyaalekhya@users.noreply.github.com>
2025-01-03 19:57:49 +0530

    Update views.py

--- quiz/views.py
+++ quiz/views.py
@@ -537,7 +537,7 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 
-net = cv2.dnn.readNet("templates/quiz/yolov4-tiny.weights", "templates/quiz/yolov4-tiny.cfg")
+net = cv2.dnn.readNet("quiz/templates/quiz/yolov4-tiny.weights", "quiz/templates/quiz/yolov4-tiny.cfg")
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
