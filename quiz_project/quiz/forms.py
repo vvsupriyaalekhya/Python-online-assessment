695660766f19a2306a567bd4ece232b64c200e47
vvsupriyaalekhya <127216907+vvsupriyaalekhya@users.noreply.github.com>
2025-01-04 13:18:59 +0530

    Update forms.py

--- quiz_project/quiz/forms.py
+++ quiz_project/quiz/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Users
 
 class LoginForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
-        model = User
+        model = Users
         fields = ['username', 'password']
